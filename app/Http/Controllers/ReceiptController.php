56848c62a7b9721617bc3968d23905c4b132b1e8
vagrant <vagrant@homestead>
2024-02-01 17:37:40 +0000

    receipts 1

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -1570,12 +1570,11 @@ class ReceiptController extends Controller
         //start OCR
         $ch = curl_init();
         $post = $post_data;
-        dd([$document_model, $doc_type]);
-        $document_model = $document_model; //lol WHY!?
+        // $document_model = $document_model; //lol WHY!?
         $azure_api_key = env('AZURE_RECEIPTS_KEY');
         $azure_api_version = env('AZURE_RECEIPTS_VERSION');
-
-        curl_setopt($ch, CURLOPT_URL, "https://" . env('AZURE_RECEIPTS_URL') . "/formrecognizer/documentModels/" . $document_model . ":analyze?api-version=" . $azure_api_version . " ");
+        dd("https://" . env('AZURE_RECEIPTS_URL') . "/formrecognizer/documentModels/" . $document_model . ":analyze?api-version=" . $azure_api_version);
+        curl_setopt($ch, CURLOPT_URL, "https://" . env('AZURE_RECEIPTS_URL') . "/formrecognizer/documentModels/" . $document_model . ":analyze?api-version=" . $azure_api_version);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");

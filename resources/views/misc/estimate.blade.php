1048c37ab2cbc5b4624a3b02981adb46b4dc5b3f
vagrant <vagrant@homestead>
2024-11-22 21:12:39 +0000

    estimate print 1

--- resources/views/misc/estimate.blade.php
+++ resources/views/misc/estimate.blade.php
@@ -5,6 +5,8 @@
         <meta charset="utf-8">
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
 
     <body>

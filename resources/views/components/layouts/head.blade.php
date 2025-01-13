afc46a2d54e0f7ae17b2f9e8749ced01db89ae79
vagrant <vagrant@homestead>
2025-01-13 04:01:46 +0000

    test 1

--- resources/views/components/layouts/head.blade.php
+++ resources/views/components/layouts/head.blade.php
@@ -27,9 +27,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ isset($title) ? $title . ' | ' . env('APP_NAME') : env('APP_NAME') }}</title>
+    <title>{{ isset($title) ? $title . ' | ' . env('APP_NAME') : env('APP_NAME')}}</title>
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
-
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">

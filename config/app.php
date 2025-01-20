4b5e0318ff4b165b6a2bb66b7f066286746a6cb8
Shift <shift@laravelshift.com>
2025-01-20 19:47:22 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -7,27 +7,6 @@ return [
 
     'log_max_files' => 180,
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-        // Barryvdh\Snappy\ServiceProvider::class,
-        Weidner\Goutte\GoutteServiceProvider::class,
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'Goutte' => Weidner\Goutte\GoutteFacade::class,

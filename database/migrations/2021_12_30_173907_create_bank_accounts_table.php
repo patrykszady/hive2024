e359f020e3ab62c6eddecbf9e5e1c8d27a6032ea
Shift <shift@laravelshift.com>
2025-01-20 19:47:25 +0000

    Adopt anonymous migrations

--- database/migrations/2021_12_30_173907_create_bank_accounts_table.php
+++ database/migrations/2021_12_30_173907_create_bank_accounts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBankAccountsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -34,4 +34,4 @@ class CreateBankAccountsTable extends Migration
     {
         Schema::dropIfExists('bank_accounts');
     }
-}
+};

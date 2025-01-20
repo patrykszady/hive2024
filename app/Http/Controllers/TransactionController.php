ca1f4520bd61bde69a44b3aa9e9f37c30e4e8312
vagrant <vagrant@homestead>
2025-01-20 19:17:15 +0000

    transaction checks 1

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -1354,7 +1354,6 @@ class TransactionController extends Controller
                 ->whereNull('deleted_at')
                 ->where('date', '>', '2021-01-01')
                 ->orderBy('date', 'DESC')
-
                 // ->where('id', 3244)
                 ->get();
 
